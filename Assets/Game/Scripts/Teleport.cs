4c6aa30601c58e15bc84381dd69d97676b0b326a
fiwon123 <felipeinoue@yahoo.com.br>
2020-04-16 03:50:54 -0300

    Connect all places

--- Assets/Game/Scripts/Teleport.cs
+++ Assets/Game/Scripts/Teleport.cs
@@ -7,10 +7,25 @@ public class Teleport : MonoBehaviour
 	public TeleportManager.placesName nextPlace;
 	public bool exactPosition = true;
 	public bool isX, isY;
+	public bool isInverted = false;
+	public bool isChangeValue = false;
 
 	private void OnTriggerEnter2D(Collider2D collision) {
-		float xValue = (isX && !exactPosition) | exactPosition ? -1f : Player.instance.gameObject.transform.position.x;
-		float yValue = (isY && !exactPosition) | exactPosition ? -1f : Player.instance.gameObject.transform.position.y;
+		float xValue, yValue;
+
+		if (isInverted && !isChangeValue) {
+			xValue = (isX && !exactPosition) | exactPosition ? -1f : Player.instance.gameObject.transform.position.y;
+			yValue = (isY && !exactPosition) | exactPosition ? -1f : Player.instance.gameObject.transform.position.x;
+
+		} else if (isInverted && isChangeValue) {
+			xValue = (isX && !exactPosition) | exactPosition ? -1f : -Player.instance.gameObject.transform.position.y;
+			yValue = (isY && !exactPosition) | exactPosition ? -1f : -Player.instance.gameObject.transform.position.x;
+		}
+		else {
+			xValue = (isX && !exactPosition) | exactPosition ? -1f : Player.instance.gameObject.transform.position.x;
+			yValue = (isY && !exactPosition) | exactPosition ? -1f : Player.instance.gameObject.transform.position.y;
+		}
+
 		FindObjectOfType<TeleportManager>().TeleportTo(nextPlace, xValue, yValue);
 	}
 }
