aeb8f8a5fbeeff93a19d90a2107115116976b597
fiwon123 <felipeinoue@yahoo.com.br>
2020-04-17 21:05:45 -0300

    Put Walls in game

---
+++ Assets/Lake.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+using Yarn.Unity.Example;
+
+public class Lake : MonoBehaviour
+{
+	bool canInteract = false;
+
+	private void Update() {
+		if (Input.GetKeyDown(KeyCode.Z) && canInteract) {
+			if (Player.instance.HasNote()) {
+				GameManager.instance.completedIce = true;
+				Player.instance.DestroyNote();
+			} else {
+				FindObjectOfType<DialogueRunner>().StartDialogue(GetComponent<NPC>().talkToNode);
+			}
+		}
+	}
+
+	private void OnTriggerStay2D(Collider2D collision) {
+		if (collision.tag == "Player") {
+			canInteract = true;
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D collision) {
+		if (collision.tag == "Player") {
+			canInteract = false;
+		}
+	}
+}
